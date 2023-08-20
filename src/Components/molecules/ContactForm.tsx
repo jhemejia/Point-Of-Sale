571e0f04edd8fa30e9668d3f959aeae858d25688
JhersonMejia <jhemejia95@gmail.com>
2023-08-19 19:41:05 -0500

    Added Contact form Widget, 2 sections for LP

---
+++ src/Components/molecules/ContactForm.tsx
@@ -0,0 +1,202 @@
+import { useState, useEffect, useRef } from 'react'
+import { Link } from 'react-router-dom'
+
+interface FormData {
+    name: string;
+    email: string;
+    message: string;
+  }
+
+const ContactForm = () => {
+
+    const [open, setOpen] = useState<boolean>(false)
+    const widgetRef = useRef<HTMLDivElement>(null)
+    const [formValues, setFormValues] = useState<FormData>({
+        name: '',
+        email: '',
+        message: '',
+      })
+    // function to close the contact form widget when clicking outside of the form
+    const handleClickOutside = (event:any)=>{
+        if(open && widgetRef.current && !widgetRef.current.contains(event.target as Node) )
+        {
+            setOpen(false)
+        }
+    }
+    useEffect(()=>{
+        document.addEventListener("click",handleClickOutside)
+        return (
+          ()=> document.removeEventListener("click",handleClickOutside)
+        )
+    },[open])
+
+    //function to handle input changes
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { name, value } = event.target;
+        setFormValues((prevFormData) => ({
+          ...prevFormData,
+          [name]: value,
+        }));
+      };
+
+    const handleFormSubmit = async (e:any)=>{
+        e.preventDefault()
+        const formData = new FormData(e.target)
+
+        formData.append("access_key", "33d86185-c2dc-459e-9f63-0af715df2d21");
+
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+        try{
+        await fetch("https://api.web3forms.com/submit",{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+              },
+              body: json
+        })
+        setOpen(false)
+        setFormValues({
+            name: '',
+            email: '',
+            message: '',
+          })
+    } catch (error){
+        console.log("Contact Form Error", error)
+    }    
+    }
+
+  return (
+    <div className="bg-white dark:bg-grey-900 mx-6">
+      {/* Contact Form component */} 
+    {/* Web3Forms Popup Widget Starts here */} 
+        <div  id="web3forms__widget" ref={widgetRef}> 
+            <div
+                id="w3f__widget--content"
+                className={["fixed flex flex-col z-50 bottom-[100px] top-0 right-0 h-auto left-0 sm:top-auto sm:right-5 sm:left-auto h-[calc(90%-75px)] w-full sm:w-[350px] overflow-auto  border border-grey-300 bg-white shadow-2xl rounded-md ",open?"":"hidden"].join(" ")}
+                
+            >
+                <div className="flex p-2 flex-col justify-center items-center h-32 bg-indigo-600">
+                    <h3 className="text-lg text-white">How can I help?</h3>
+                    <p className="text-white opacity-50">I usually respond in a few hours</p>
+                </div>
+                <div className="bg-grey-50 flex-grow py-3 px-5"> 
+                    <form
+                    onSubmit={handleFormSubmit}
+                    action="none"
+                    id="form"
+                    >
+                        <input
+                            type="hidden"
+                            name="subject"
+                            value="New Submission from Web3Forms"
+                        />       
+                        <div className="mb-4">
+                            <label
+                                htmlFor="name"
+                                className="block mb-2 text-sm text-grey-600 dark:text-grey-400"
+                                >Full Name</label>
+                            <input
+                                type="text"
+                                name="name"
+                                id="full_name"
+                                placeholder="John Doe"
+                                required
+                                value={formValues.name}
+                                onChange={handleInputChange}
+                                className="w-full px-3 py-2 bg-white placeholder-grey-300 border border-grey-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300"
+                            />
+                        </div>
+                        
+                        <div className="mb-4">
+                            <label
+                                htmlFor="email"
+                                className="block mb-2 text-sm text-grey-600 dark:text-grey-400"
+                                >Email Address</label>
+                            <input
+                                type="email"
+                                name="email"
+                                id="email"
+                                placeholder="you@company.com"
+                                value={formValues.email}
+                                onChange={handleInputChange}
+                                required
+                                className="w-full px-3 py-2 bg-white placeholder-grey-300 border border-grey-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300"
+                            />
+                        </div>
+                    
+                        <div className="mb-4">
+                            <label
+                            htmlFor="message"
+                            className="block mb-2 text-sm text-grey-600 dark:text-grey-400"
+                            >Your Message</label>
+
+                            <textarea
+                            rows={3}
+                            name="message"
+                            id="message"
+                            placeholder="Your Message"
+                            value={formValues.message}
+                            onChange={handleInputChange}
+                            className="w-full h-20 px-3 py-2 bg-white placeholder-grey-300 border border-grey-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300"
+                            required
+                            ></textarea>
+                        </div>
+                        <div className="mb-3">
+                            <button
+                            type="submit"
+                            className="w-full px-3 py-4 text-white bg-indigo-500 rounded-md focus:bg-indigo-600 focus:outline-none"
+                            >
+                            Send Message
+                            </button>
+                        </div>
+                        <p className="text-xs text-center text-grey-400" id="result">
+                            <span>Powered by <Link to="https://Web3Forms.com" className="text-grey-600" target="_blank" rel="noopener noreferrer">Web3Forms</Link></span>
+                        </p>
+                    </form>
+                </div>
+            </div>
+            <button
+                onClick={()=>setOpen(!open)}
+                className="fixed z-40 right-5 bottom-5 shadow-lg flex justify-center items-center w-14 h-14 bg-indigo-500 rounded-full focus:outline-none hover:bg-indigo-600 focus:bg-indigo-600 transition duration-300 ease"
+            >
+                <svg
+                className={["w-6 h-6 text-white absolute", open?"hidden":""].join(" ")}
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                >
+                <path
+                    d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"
+                ></path>
+                </svg>
+                <svg
+                className={["w-6 h-6 text-white absolute", open?"":"hidden"].join(" ")}
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                >
+                <line x1="18" y1="6" x2="5" y2="19"></line>
+                <line x1="6" y1="6" x2="18" y2="17"></line>
+                </svg>
+            </button>
+        </div>
+    {/* Web3Forms Popup Widget End here */}
+    </div>
+  )
+}
+
+export default ContactForm
--- src/Components/pages/Home.tsx
+++ src/Components/pages/Home.tsx
@@ -1,5 +1,6 @@
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Link, Outlet, useNavigate } from 'react-router-dom';
 import Footer from '../molecules/Footer';
+import ContactForm from '../molecules/ContactForm';
 
 const home = () => {
   const navigate = useNavigate();
@@ -7,17 +8,17 @@ const home = () => {
     <>
     <div className='flex flex-col items-center justify-between flex-nowrap h-full max-w-screen'>
       {/*<!-- Header Menu Left -->*/}    
-      <header className="w-full mt-1 text-gray-700 bg-white shadow-sm body-font relative">
-        <div className="container flex flex-col items-center justify-between p-2 mx-auto md:flex-row">
-            <a href="/" className="flex items-center font-medium text-gray-900 title-font md:mb-0">
+      <header className="w-full mt-1 text-gray-700 bg-white shadow-sm body-font relative ">
+        <div className="container flex items-center justify-between p-2 mx-auto max-w-full">
+            <Link to="/" className="flex items-center font-medium text-gray-900 title-font md:mb-0">
                 <img src="./logo.png" alt="logo" className="w-20 " />
-            </a>
-            <nav className="flex flex-wrap items-center justify-center pl-6 ml-6 text-base border-l border-gray md:mr-auto">
-                <a href="#_" className="mr-5 font-medium hover:text-gray-900">Home</a>
-                <a href="#_" className="mr-5 font-medium hover:text-gray-900">About</a>
-                <a href="#_" className="font-medium hover:text-gray-900">Contact</a>
+            </Link>
+            <nav className="flex-nowrap items-center justify-center pl-6 ml-6 text-base border-l hidden sm:flex border-gray md:mr-auto ">
+                <Link to="/" className="mr-5 font-medium hover:text-gray-900">Home</Link>
+                <Link to="/about" className="mr-5 font-medium hover:text-gray-900">About</Link>
+                <Link to="/contact" className="font-medium hover:text-gray-900">Contact</Link>
             </nav>
-            <div className="items-center h-full justify-end">
+            <div className="flex flex-nowrap items-center h-full justify-end">
                 <button onClick={()=>navigate("/login")} className="mr-5 font-medium hover:text-gray-900">Login</button>
                 <button onClick={()=>navigate("/signup")}
                     className="px-4 py-2 text-xs font-bold text-white uppercase transition-all duration-150 bg-green-500 rounded shadow outline-none active:bg-green-600 hover:shadow-md focus:outline-none ease">
@@ -27,7 +28,8 @@ const home = () => {
         </div>
       </header>
       <div className="h-full max-w-full relative ">
-        <Outlet/>
+        <Outlet/> 
+        <ContactForm />
       </div>
       <div className='relative mt-2 w-full'>
       <Footer />
