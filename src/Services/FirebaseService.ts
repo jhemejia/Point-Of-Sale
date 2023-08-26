8bca3bacc3d97c5bebce951fca7ef7a7c4df8418
JhersonMejia <jhemejia95@gmail.com>
2023-08-25 23:12:58 -0500

    init firebase and firebase auth services

---
+++ src/Services/FirebaseService.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+    apiKey: "API_KEY",
+    authDomain: "PROJECT_ID.firebaseapp.com",
+    // The value of `databaseURL` depends on the location of the database
+    databaseURL: "https://DATABASE_NAME.firebaseio.com",
+    projectId: "PROJECT_ID",
+    storageBucket: "PROJECT_ID.appspot.com",
+    messagingSenderId: "SENDER_ID",
+    appId: "APP_ID",
+};
+
+export const app = initializeApp(firebaseConfig);
