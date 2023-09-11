0d2dd7715b457bfd61a74c9f4099ba3c53248b35
JhersonMejia <jhemejia95@gmail.com>
2023-09-10 19:22:54 -0500

    products filter and sort

--- src/Components/organisms/ProductsGrid.tsx
+++ src/Components/organisms/ProductsGrid.tsx
@@ -8,13 +8,12 @@ interface ProductsGridProps {
 
 const ProductsGrid = (props:ProductsGridProps) => {
 
-  const [ sort, setSort ] = useState('recommended')
+  const [ sort, setSort ] = useState('0')
   const [ categories, setCategories ] = useState<any[]|undefined>([])
   const [ selectedCategory, setSelectedCategory ] = useState("")
 
   const getProductCategories = (items: Item[] | undefined) => {
     const categoriesSet = new Set();
-  
     if (items) {
        items.filter(item => {
         if (!categoriesSet.has(item.category.name)) {
@@ -23,29 +22,39 @@ const ProductsGrid = (props:ProductsGridProps) => {
         }
         return false;
       });
-  
       const categories = Array.from(categoriesSet);
-  
       return categories
     }
-  
     return [];
   };
-  
 
  useEffect(()=>{
   setCategories(getProductCategories(props.items))
  },[props.items])
 
- const filterItems = (items: Item[]|undefined )=>{
-    const filteredItems = items?.filter((item:Item)=>{
-    if(selectedCategory == "" || item.category.name == selectedCategory){
-      return true
-    } 
-    return false
-   })
-   return filteredItems
- }
+ const filterAndSortItems = (items: Item[] | undefined) => {
+    if (!items) {
+      return [];
+    }
+    // Filter items based on selectedCategory
+    const filteredItems = items.filter((item: Item) => {
+      if (selectedCategory === "" || item.category.name === selectedCategory) {
+        return true;
+      }
+      return false;
+    });
+    // Sort the filtered items based on the "sort" parameter
+    const sortedItems = [...filteredItems]; // Create a shallow copy to avoid mutating the original array
+    if (sort === "0") {
+      // Sort in ascending order of price
+      sortedItems.sort((a, b) => a.price - b.price);
+    } else if (sort === "1") {
+      // Sort in descending order of price
+      sortedItems.sort((a, b) => b.price - a.price);
+    }
+    return sortedItems;
+  };
+  
   return (
     <section className="bg-white dark:bg-gray-900 flex mx-auto w-full">
         <div className=" px-6 py-8 mx-auto flex w-full">
@@ -68,18 +77,18 @@ const ProductsGrid = (props:ProductsGridProps) => {
 
                 <div className="mt-6 lg:mt-0 lg:px-2 lg:w-4/5 ">
                     <div className="flex items-center justify-between text-sm tracking-widest uppercase ">
-                        <p className="text-gray-500 dark:text-gray-300">{filterItems(props.items as Item[])?.length} Items</p>
+                        <p className="text-gray-500 dark:text-gray-300">{filterAndSortItems(props.items as Item[])?.length} Items</p>
                         <div className="flex items-center">
                             <p className="text-gray-500 dark:text-gray-300">Sort</p>
                             <select value={sort} onChange={e=>setSort(e.target.value)} className="font-medium text-gray-700 bg-transparent dark:text-gray-500 focus:outline-none">
-                                <option value="recommended">Recommended</option>
-                                <option value="price">Lowest Price</option>
+                                <option value="0">Lowest Price</option>
+                                <option value="1">Highest Price</option>
                             </select>
                         </div>
                     </div>
 
                     <div className="grid grid-cols-1 gap-8 mt-8 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4">
-                       {filterItems(props.items as Item[])?.map((item: Item, index:number)=>{
+                       {filterAndSortItems(props.items as Item[])?.map((item: Item, index:number)=>{
                          return (
                            <ProductCard {...item} key={index}/>
                            )
